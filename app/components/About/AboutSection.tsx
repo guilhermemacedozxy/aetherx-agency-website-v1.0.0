770879253a5e97bd0f333488f7b67352b6c78453
guilhermemacedozxy <contato.guilhermemacedo.dev@gmail.com>
2026-06-10 21:56:27 -0300

    style: Reduz gap entre título e descrição das features para 2

--- app/components/About/AboutSection.tsx
+++ app/components/About/AboutSection.tsx
@@ -85,7 +85,7 @@ export default function AboutSection() {
             {FEATURES.map((feature) => (
               <div key={feature.title} className="flex items-center gap-6">
                 <FeatureIcon src={feature.icon} />
-                <div className="flex flex-col gap-3">
+                <div className="flex flex-col gap-2">
                   <h3 className="text-[20px] font-semibold text-black uppercase">
                     {feature.title}
                   </h3>
